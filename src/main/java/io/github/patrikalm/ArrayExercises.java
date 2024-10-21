ba41fa4c7b3f152ac35f47be8a0e7cab0211e213
Patrik Alm <almpatrik@yahoo.se>
2024-10-21 13:33:31 +0200

    Added class ArrayExercises and coded exercise 1

---
+++ src/main/java/io/github/patrikalm/ArrayExercises.java
@@ -0,0 +1,23 @@
+package io.github.patrikalm;
+
+
+public class ArrayExercises {
+
+    public static void main(String[] args) {
+
+        ex1();
+
+    }
+
+    public static void ex1() {
+
+        int[] numbers = {11, 23, 29};
+
+        for (int element : numbers) {
+
+            System.out.print(element + " ");
+
+        }
+    }
+
+}
